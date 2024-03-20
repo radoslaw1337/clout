290bb9a6709a007f176fbc82f8c0dd9c136649e1
radoslaw1337 <radek7861@gmail.com>
2024-03-20 17:38:35 +0100

    3.0

--- st/main.js
+++ st/main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Jul 13, 2024 00:00:00").getTime();
+let launchDate = new Date("Jul 8, 2023 00:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
